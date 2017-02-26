52e0dad49530d7f8255eec81bfa828c364d34d29
MFortier <MFortier21github@gmail.com>
2017-02-26 12:49:03 -0700

    Started HTML scaffolding, basic styles/scripts importing. Still cannot get grunt/gulp/npm to work correctly/consistently.

---
+++ solution/CourseProjectApp/src/ToDoWebApi/Models/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ToDoWebApi.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(ToDoItem item);
+        IEnumerable<ToDoItem> GetAll();
+        ToDoItem Find(long key);
+        void Remove(long key);
+        void Update(ToDoItem item);
+    }
+}
---
+++ solution/CourseProjectApp/src/ToDoWebApi/Models/ToDoRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoWebApi.Models
+{
+    public class ToDoRepository : ITodoRepository
+    {
+        private readonly TodoContext _context;
+
+        public TodoRepository(TodoContext context)
+        {
+            _context = context;
+            Add(new ToDoItem { Name = "Item1" });
+        }
+
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        public void Add(ToDoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+        }
+
+        public ToDoItem Find(long key)
+        {
+            return _context.TodoItems.FirstOrDefault(t => t.Key == key);
+        }
+
+        public void Remove(long key)
+        {
+            var entity = _context.TodoItems.First(t => t.Key == key);
+            _context.TodoItems.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(ToDoItem item)
+        {
+            _context.TodoItems.Update(item);
+            _context.SaveChanges();
+        }
+    }
+}
