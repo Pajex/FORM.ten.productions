3cb60dcf71ac1eddadfdea992c325de5080d22f3
MFortier <MFortier21github@gmail.com>
2017-02-25 21:53:26 -0700

    Completed basic EF Core tutorial. Renamed project folder to FormTenProductions.

---
+++ solution/MvcMovie/src/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    Console.WriteLine("Database has already been seeded.");
+                    return; // DB has been seeded.
+                }
+
+                Console.WriteLine("Beginning database seed.");
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-1-11"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "A14",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "A",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "PG",
+                        Price = 3.99M
+                    });
+
+                Console.WriteLine("Database seed complete.");
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
